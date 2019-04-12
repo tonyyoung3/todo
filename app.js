6c8e98356afd277f7e4fe22e2605eb9c50c7c64e
tonyyoung3 <tonyyoung3@gmail.com>
2019-04-12 21:16:15 +0800

    Add Mongoose connection and create Todo model

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express')               // 載入 express
-const app = express()                                     // 啟用 express
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')                    // 載入 mongoose
 
-// 設定第一個首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })   // 設定連線到 mongoDB
+
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
 })
 
-// 設定 express port 3000
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Todo = require('./models/todo');
+
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
 app.listen(3000, () => {
-  console.log('App is running')
+  console.log('App is running!')
 })
