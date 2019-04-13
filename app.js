519e76eedb7ee199df8417a1303fccbbb24403a2
tonyyoung3 <tonyyoung3@gmail.com>
2019-04-13 09:58:57 +0800

    Add index action and index view template

--- app.js
+++ app.js
@@ -28,8 +28,12 @@ const Todo = require('./models/todo');
 
 // 設定路由
 // Todo 首頁
+// Todo 首頁
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  Todo.find((err, todos) => {                                 // 把 Todo model 所有的資料都抓回來
+    if (err) return console.error(err)
+    return res.render('index', { todos: todos })  // 將資料傳給 index 樣板
+  })
 })
 
 // 列出全部 Todo
