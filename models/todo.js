42eafc200db7a3cb17690b7f8498087c261278b5
tonyyoung3 <tonyyoung3@gmail.com>
2019-04-17 20:28:27 +0800

    Add Bootstrap

--- models/todo.js
+++ models/todo.js
@@ -5,7 +5,11 @@ const todoSchema = new Schema({
   name: {
     type: String,               // 資料型別是字串
     required: true,          // 這是個必填欄位
-  }
+  },
+  done: {                             // 「完成狀態」
+    type: Boolean,
+    default: false,               // 預設完成狀態為 false
+  },
 })
 
 module.exports = mongoose.model('Todo', todoSchema)
